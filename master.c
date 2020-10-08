a60866063be597a8fdb0c8b209fbcf0a3c6824a8
JesLaz <38595549+JesLaz@users.noreply.github.com>
2020-10-08 06:04:08 +0000

    Update files

--- master.c
+++ master.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <errno.h>
 #include <sys/ipc.h>
 #include "myShm.h"
@@ -17,54 +18,78 @@ int main(int argc, char *argv[]){
 	
 	int shm_fd; /*file descriptor, from shm_open()*/
 	struct CLASS *shm_base; /*base address, from mmap()*/
-	void *ptr; /*shm_base is fixed, ptr is movable*/
+	//void *ptr; /*shm_base is fixed, ptr is movable*/
 
 	/*create the shared memory segment*/
 	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+	//error handling
 	if(shm_fd == -1){
 		printf("Master: Shared memory failed: %s\n", strerror(errno));
 		exit(1);
 	}
-	/*Configure the size of the shared memroy segment*/
+	/*Configure the size of the shared memory segment*/
 	ftruncate(shm_fd, SIZE);
 
 	/*Map shared memory segment in the address space of the process*/
 	shm_base = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd,0);
-	shm_base->index = 0;
+
+	//error handling
 	if(shm_base == MAP_FAILED){
 		printf("Master: Map failed: %s\n", strerror(errno));
 		/*close and shm_unlink*/
 		exit(1);
 	}
-	
-	printf("Master: %d", shm_fd);
-	//Write into shared memory to attempt to read from it from the slave process it's passed into
-	int number;
-	//sprintf(ptr, 6);
-	//ptr += sizeof(int);
-	int x;
-	printf("Enter the number %d ", x);
-	scanf("%d", &(shm_base->response[0]));
-	printf("Number entered: %d: ", shm_base->response[0]);
-	shm_base->index += 1;
 
-	pid_t cpid = fork();
-	if(cpid < 0){
-		printf("Fork failed");
-		return 1;
-	}
-	if(cpid == 0){
-		int num = 5;
-		char snum[5];
-		sprintf(snum, "%d", x);
-		char const *cNum = snum;
-		printf("Enters child");
-		execlp("./slave", "slave", cNum, NULL);
-		exit(0);
-	}
+	pid_t pid;
+	printf("Master created %s", argv[1]);
+	printf(" child processes to execute slave\n");
+	int y = atoi(argv[1]);
 	/*Now write into the shared memory region by running through the child process*/
-	/*Must increment the value of the pointer after each write*/
+	for(int i = 0; i < y; i++){
+		pid = fork();
+		if(pid < 0){
+			printf("Fork failed");
+			return 1;
+		}
+		if(pid == 0){
+			int num = i+1;
+			char snum[5];
+			sprintf(snum, "%d", num);
+			printf("Enters child");
+			char *cmd = "./slave";
+			char *argv[4];
+			argv[0] = "./slave";
+			argv[1] = "my_shm";
+			argv[2] = snum;
+			argv[3] = NULL;
+			execvp(cmd, argv);
+			exit(0);
+		}
+		else{
+			//Allow child processes to terminate before parent process continues
+			wait(NULL);
+		}
+	}
+	printf("Master recived termination signals from all %s", argv[1]);
+	printf(" processes\n");
+	printf("Content of shared memory filled by child processes:\n");
+	for(int i = 0; i < y; i++){
+		printf("Array element %d", i);
+		printf(" : %d", shm_base->response[i]);
+		printf("\n");
+	}
 	
+	
+	printf("Master removed shared memory segment, and is exiting\n");
 	//Attempt to unlink at the end
+	if(munmap(shm_base, SIZE) == -1){
+		printf("prod: Unmap failed: %s\n", strerror(errno));
+		exit(1);
+	}
+	/* close shared memory segment as if it was a file */
+	if (close(shm_fd) == -1){
+		printf("prod: Close failed: %s\n", strerror(errno));
+		exit(1);
+	}
 	return 0;
 }
--- slave.c
+++ slave.c
@@ -12,11 +12,42 @@
 #include "myShm.h"
 
 int main(int argc, char *argv[]){
-	printf("Inside the slave process: ");
-	printf("%s", argv[1]);
-	const char* name = "my_shm";
-	const int SIZE = 4096;
-	int shm_fd;//File descriptor
-	printf("Ending the slave process");
+	printf("Slave begins execution\n");
+	printf("I am child number %s", argv[2]);
+	printf(", received memory name %s", argv[1]);
+
+	const int SIZE = 4096;//File size
+	/*As slave will do writing into the shared memory shm_open will be called with the flag
+	O_RDWR as opposed to O_RDONLY otherwise write permission will not be granted*/
+	int shm_fd = shm_open(argv[1], O_CREAT | O_RDWR, 0666);
+	struct CLASS *shm_base; //Base address from mmap()
+	//error handling
+	if(shm_fd == -1){
+		printf("Slave: shared memory failed: %s\n", strerror(errno));
+		exit(1);
+	}
+	shm_base = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if(shm_base == MAP_FAILED){
+		printf("Slave: Map failed: %s\n", strerror(errno));
+		exit(1);
+	}
+	
+	int x = atoi(argv[2]);//Convert the string argument to an int as response in int[]
+	//Assign the the value of the child number to the response array
+	shm_base->response[x-1] = x;
+
+	printf("\nI have written my child number to shared memory\n");
+	//Must increment the value of the pointer after each write
+	shm_base->index ++;
+	//Unmap and close access to the shared memory before the child process finishes
+	if(munmap(shm_base, SIZE) == -1){
+		printf("Slave: Unmap failed %s\n", strerror(errno));
+		exit(1);
+	}
+	if(close(shm_fd) == -1){	
+		printf("Slave: Close failed: %s\n", strerror(errno));	
+		exit(1);
+	}
+	printf("Slave closed access to shared memory and terminates\n");
 	return 0;
 }
