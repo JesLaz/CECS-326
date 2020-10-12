529f6817f8f17ac362a73c24d015436d416e2580
JesLaz <38595549+JesLaz@users.noreply.github.com>
2020-10-12 19:05:22 +0000

    Update and debug files

--- shmc1.cpp
+++ shmc1.cpp
@@ -1,5 +1,6 @@
 #include "registration.h"
 #include <sys/types.h>
+#include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -7,6 +8,8 @@
 #include <iostream>
 #include <stdio.h>
 #include <memory.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 using namespace std;
 
@@ -14,22 +17,31 @@ CLASS *class_ptr;
 void *memptr;
 char *pname;
 int shmid, ret;
+sem_t *sem; //Named Semaphore
+char *semName; //Name for semaphore
 void rpterror(char *), srand(), perror(), sleep();
-void sell_seats();
+void sell_seats(); //sell_seats() prototypes
 
-int main(int argc, char* argv[]){
-	if(argc < 2){
+main(int argc, char* argv[]){
+	if(argc < 3){
 		fprintf(stderr, "Usage:, %s shmid\n", argv[0]);
 		exit(1);
 	}
 	pname = argv[0];
 	sscanf (argv[1], "%d", &shmid);
+	semName = argv[2];
+
 	memptr = shmat(shmid, (void *)0, 0);
+
 	if(memptr == (char *)-1){
 		rpterror((char *)"shmat failed");
 		exit(2);
 	}
 	class_ptr = (struct CLASS *)memptr;
+	if((sem = sem_open(semName, 0)) == SEM_FAILED){
+		perror("Semaphore failed");
+		exit(1);
+	}
 	sell_seats();
 	ret = shmdt(memptr);
 	exit(0);
@@ -50,6 +62,7 @@ void sell_seats(){
 			all_out++;
 			cout << pname << " sees no seats left" << endl;
 		}
+		sem_post(sem);
 		sleep((unsigned)rand()%10 + 1);
 	}
 }
--- shmp1.cpp
+++ shmp1.cpp
@@ -8,23 +8,37 @@
 #include <iostream>
 #include <stdio.h>
 #include <memory.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <semaphore.h>
 
 using namespace std;
 
 CLASS myCLASS = {"4321", "082620", "Operating Systems", 20};
 #define NCHILD 3
+#define SNAME "shmpSem"
+
 int shm_init(void *);
-void wait_and_wrap_up(int [], void*, int);
+void wait_and_wrap_up(int [], void*, int, sem_t *);
 void rpterror(char *, char *);
 
-int main(int argc, char *argv[]){
+
+main(int argc, char *argv[]){
 	int child[NCHILD], i , shmid;
 	void *shm_ptr;
 	char ascshmid[10], pname[14];
 
+	sem_t *sem;
+	void *semPtr;
 	shmid = shm_init(shm_ptr);
 	sprintf(ascshmid, "%d", shmid);
-
+	sem = sem_open(SNAME, O_CREAT, 0644, 1);
+	
+	if(sem == SEM_FAILED){
+		perror("Semaphore Init Failed");
+		exit(1);
+	}
+	
 	for(int i = 0; i < NCHILD; i++){
 		child[i] = fork();
 		switch (child[i]){
@@ -34,30 +48,33 @@ int main(int argc, char *argv[]){
 				exit(1);
 			case 0:
 				sprintf(pname, "shmc%d", i+1);
-				execl("shmc1", pname, ascshmid, (char *)0);
+				execl("shmc1", pname, ascshmid, SNAME, (char *)0);
 				rpterror ((char *)"execl failed", pname);
 				exit(2);
 			}
 		}
-		wait_and_wrap_up (child, shm_ptr, shmid);
+		wait_and_wrap_up (child, shm_ptr, shmid, sem);
 	}
 
 int shm_init(void *shm_ptr){
 	int shmid;
-	smhid = shmget(ftok(".", 'u'), sizeof(CLASS), 0600 | IPC_CREAT);
+	shmid = shmget(ftok(".", 'u'), sizeof(CLASS), 0600 | IPC_CREAT);
+
 	if (shmid == -1){
 		perror("shmget failed");
 		exit(3);
 	}
 	shm_ptr = shmat(shmid, (void *)0, 0);
+
 	if(shm_ptr == (void *)-1){
 		perror("shmat failed");
 		exit(4);
 	}
-	memcpy(shm_ptr, (void *) &myclass, sizeof(CLASS));
+	memcpy(shm_ptr, (void *) &myCLASS, sizeof(CLASS));
+	return(shmid);
 }
 
-void wait_and_wrap_up(int child[], void *shm_ptr, int shmid){
+void wait_and_wrap_up(int child[], void *shm_ptr, int shmid, sem_t *sem){
 	int wait_rtn, w, ch_active = NCHILD;
 
 	while(ch_active > 0){
@@ -72,6 +89,8 @@ void wait_and_wrap_up(int child[], void *shm_ptr, int shmid){
 	cout << "Parent removing shm" << endl;
 	shmdt(shm_ptr);
 	shmctl(shmid, IPC_RMID, (struct shmid_ds *) 0);
+	sem_unlink(SNAME);
+	sem_close(sem);
 	exit(0);
 }
 
