a84a13ed58672e46d5a35c1c5dfeb3d14a0ea9cf
Jessie Lazo <38595549+JesLaz@users.noreply.github.com>
2020-11-01 18:21:29 -0800

    Create transfer.c

---
+++ Assignment 7/transfer.c
@@ -0,0 +1,22 @@
+#include "accounts.h"
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <memory.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+#Jessie Lazo
+using namespace std;
+
+main(int argc, char *argv[]){
+  //do stuff
+  exit(0);
+}
