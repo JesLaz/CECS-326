e9d69870e62b82fa850c16c921395b92c6d59507
Jessie Lazo <38595549+JesLaz@users.noreply.github.com>
2020-12-08 22:20:41 -0800

    Update accounts.h

--- Assignment 9/accounts.h
+++ Assignment 9/accounts.h
@@ -5,11 +5,4 @@ struct ACCOUNTS{
 	int accounts[50]; /* space to hold up to 50 accounts */
 	sem_t accountSems[50]; /*Create an array of semaphores so that children won't need to be 
 		passed the name of the semaphores*/
-	/*
-	nAccounts = 2
-	accounts[200, 400, NUll NULL NULLL ...]
-	
-	sem_wait(accoutSems[0])
-	sem_post(accountSems[0])
-	*/
 };
